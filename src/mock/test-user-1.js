c61fe134fd9455d96e666f2e506eefff5bff4de4
Ana Luczynski <ana.luczynski@salesforce.com>
2018-05-02 15:16:19 -0500

    create default data

---
+++ src/mock/test-user-1.js
@@ -0,0 +1,11 @@
+export default {
+  // TODO these should come from the uPort profile
+  firstName: 'Bana',
+  lastName: 'Bofana',
+  
+  city: '',
+  state: '',
+  country: '',
+  hobbies: '',
+  causes: ''
+}
