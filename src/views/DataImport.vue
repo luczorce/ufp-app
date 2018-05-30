d4bb75ad27ca56cb7eb168841e8bf36c7857e652
Ana Luczynski <ana.luczynski@salesforce.com>
2018-05-30 09:21:24 -0500

    new Placeholder routes

---
+++ src/views/DataImport.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page data-import-page">
+    <h1>Import Data</h1>
+
+    <p>Choose from these participating companies to import your data. This is a transfer of ownership, and you should always review what you share.</p>
+    
+    <ul>
+      <li>Brand B</li>
+      <li>Wonder Brand</li>
+      <li>Brand Boys</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'dataimportview'
+}
+</script>
+
+<style>
+</style>
